869db91a807e54924f2f619dfe47fa7bc15ed0b6
DamaraLucenae <damara.ti.dev@gmail.comm>
2023-05-27 17:52:07 -0300

    Add Pre-load assets: gif

---
+++ script.js
@@ -0,0 +1,11 @@
+// Loader
+
+window.addEventListener('load', function() {
+    var preLoader = document.querySelector('.pre-loader');
+      setTimeout(function() {
+        preLoader.style.display = 'none';
+      }, 1800);
+});
+
+// Path: Tic-Tac-Toe\script.js
+
